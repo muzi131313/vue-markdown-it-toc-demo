3ac031f2bac6ba03abcf193ecc054959d4fd4111
muzi131313 <muzi131313@163.com>
2019-10-31 17:41:32 +0800

    fix: remove spare link

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <div id="app">
     <div id="nav">
       <!-- <img :src="`${$BASE_URL}/favicon.ico`" alt=""> -->
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
     </div>
     <router-view/>
   </div>
